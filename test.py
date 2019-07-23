6b28ed8fd0d0f7a6f7a1f18194e400228ad3b5b1
Taliesin Beynon <tali@taliesin.ai>
2019-07-23 21:54:44 +0200

    Integrate simple tensorboard logging.

--- test.py
+++ test.py
@@ -7,6 +7,8 @@ from data import mnist_generator, cifar_generator
 
 from xox import XOXHyperNetwork, XOXLinear
 
+import tensorboardX
+
 
 acc_gen = mnist_generator(5, is_train=False)
 def test_accuracy(net):
@@ -47,6 +49,8 @@ num_params = sum(p.numel() for p in params)
 
 print(f"Number of parameters = {num_params}")
 
+writer = tensorboardX.SummaryWriter(log_dir="tb_log")
+
 opt = torch.optim.Adam(params, lr=0.01)
 time = 0
 for img, label in mnist_generator(64):
@@ -57,9 +61,14 @@ for img, label in mnist_generator(64):
     loss = functional.cross_entropy(label_prime, label)
     loss.backward()
     opt.step()
+    writer.add_scalar("loss", loss, time)
     if time % 500 == 0:
         if time % 1000 == 0:
             acc = test_accuracy(net)
+            writer.add_scalar("accuracy", acc, time)
             print(f"{time:>5d}\t{loss:.3f}\t{acc:.3f}")
         else:
             print(f"{time:>5d}\t{loss:.3f}")
+    if time > 5000: break
+
+writer.close()
