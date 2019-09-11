3a2738a8d5698a13a6a85156f76ed61542dd2d32
Taliesin Beynon <tali@taliesin.ai>
2019-09-11 17:47:37 +0200

    Comments.

--- mnist_curve.py
+++ mnist_curve.py
@@ -7,12 +7,57 @@ from xox import XOXHyperNetwork, HyperNetwork
 from train import train
 from cache import cached, load_cached_results_as_pandas
 
-def train_mnist_multi_layer():
-    net = nn.Sequential(
-        nn.Linear(28 * 28, 64),
-        nn.ReLU(),
-        nn.Linear(64, 10)
-    )
+import seaborn as sns
+# import pandas as pd
+import matplotlib.pyplot as plt
+
+'''
+This file computes the relationship between number of genes in a hypernetwork
+and the resulting accuracy on MNIST (after a fixed number of SGD steps).
+
+The idea is that with too few genes, there is not enough capacity in the hypernetwork
+to represent a high-performing network. But with more genes the capacity and hence final 
+accuracy improves, and eventually we reach diminishing returns.
+
+The underlying network is a single layer perceptron. 
+
+Because each training run takes a while, and we do many training runs (one for each
+number of genes), we use the @cached utility to store the results of the training run on
+disk so that we can re-run the program, and it will pick up where it dropped off without
+recalculating previously computed training runs.
+
+See cache.py for more info.
+
+Output from this script should look something like:
+
+Loading data
+Training with 1 genes
+
+Hypernetwork: 7850 -> 804
+Training 3 weights with total 804 parameters:
+[(784, 1), (10, 1), (10,)]
+  500	1.597
+        ...
+ 5000	1.489	0.407
+final accuracy = 0.407
+Hypernetwork: 7850 -> 804
+Training 3 weights with total 804 parameters:
+[(784, 1), (10, 1), (10,)]
+  500	1.621
+        ...
+        
+[ 5 of these in total ]
+
+Training with 2 genes
+        ...
+
+Training with 3 genes
+        ...
+        
+...
+
+Training with 10 genes
+'''
 
 @cached
 def train_mnist_single_layer_xox(num_genes, steps=5000):
@@ -30,14 +75,15 @@ def train_mnist_single_layer_normal(steps=5000):
     return train(net, mnist_generator, steps, log_dir=None)
 
 
-import seaborn as sns
-# import pandas as pd
-import matplotlib.pyplot as plt
+print('Loading data')
 
-print('loading data')
 for i in range(1, 10):
-    train_mnist_single_layer_xox(i)
-print('done')
+    print(f"Training with {i} genes\n")
+    for j in range(5):
+        print(f"\tRun number {j}")
+        train_mnist_single_layer_xox(i, global_seed=j)
+
+print('Done')
 
 results_xox = load_cached_results_as_pandas(train_mnist_single_layer_xox)
 results_normal = train_mnist_single_layer_normal()
@@ -55,6 +101,5 @@ ax1.axhline(y=results_normal['accuracy'], color='black', linewidth=2, alpha=.7)
 
 ax2 = plt.twinx(ax1)
 plt2 = sns.pointplot(x='num_genes', y='capacity', data=results_xox, aspect=1.4, ax=ax2, join=False, color="#bb3f3f")
-
 
 plt.show()
