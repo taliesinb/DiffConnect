3934b7e610f1f8f55347f722b7b6eef03f4fe40e
Dániel Barabási <danielbarabasi@gmail.com>
2021-01-06 09:10:33 -0500

    Update mnist_curve_param_MulitL.py

--- mnist_curve_param_MulitL.py
+++ mnist_curve_param_MulitL.py
@@ -38,27 +38,55 @@ def train_mnist_multi_layer_spatial_to_spatial_to_learned(num_genes, steps=5000)
     net = nn.Sequential(
         ParameterizedXOXLinear(GaussianExpression([28, 28]), GaussianExpression([10,10]), num_genes),
         nn.Tanh(),
-        ParameterizedXOXLinear(GaussianExpression([10, 10]), learnedExpression(10), num_genes)
+        ParameterizedXOXLinear(GaussianExpression([10, 10]), LearnedExpression(10), num_genes)
     )
     return train(net, mnist, max_batches=steps,title='gaussian_to_gaussian_to_learned', log_dir=None)
 
 @cached
+def train_mnist_multi_layer_spatial_to_spatial_to_random(num_genes, steps=5000):
+    net = nn.Sequential(
+        ParameterizedXOXLinear(GaussianExpression([28, 28]), GaussianExpression([10,10]), num_genes),
+        nn.Tanh(),
+        ParameterizedXOXLinear(GaussianExpression([10, 10]), RandomExpression(10), num_genes)
+    )
+    return train(net, mnist, max_batches=steps,title='gaussian_to_gaussian_to_random', log_dir=None)
+
+@cached
 def train_mnist_multi_layer_random_to_random_to_random(num_genes, steps=5000):
     net = nn.Sequential(
-        ParameterizedXOXLinear(randomExpression(28*28), randomExpression(10*10), num_genes),
+        ParameterizedXOXLinear(RandomExpression(28*28), RandomExpression(10*10), num_genes),
         nn.Tanh(),
-        ParameterizedXOXLinear(randomExpression(10*10), randomExpression(10), num_genes)
+        ParameterizedXOXLinear(RandomExpression(10*10), RandomExpression(10), num_genes)
     )
     return train(net, mnist, max_batches=steps,title='random_to_random_to_random', log_dir=None)
 
 @cached
 def train_mnist_multi_layer_learned_to_learned_to_learned(num_genes, steps=5000):
     net = nn.Sequential(
-        ParameterizedXOXLinear(learnedExpression(28*28), learnedExpression(10*10), num_genes),
+        ParameterizedXOXLinear(LearnedExpression(28*28), LearnedExpression(10*10), num_genes),
         nn.Tanh(),
-        ParameterizedXOXLinear(learnedExpression(10*10), learnedExpression(10), num_genes)
+        ParameterizedXOXLinear(LearnedExpression(10*10), LearnedExpression(10), num_genes)
     )
     return train(net, mnist, max_batches=steps,title='learned_to_learned_to_learned', log_dir=None)
+
+@cached
+def train_mnist_multi_layer_learned_gaussian_to_learned_gaussian_to_learned(num_genes, steps=5000):
+    net = nn.Sequential(
+        ParameterizedXOXLinear(LearnedGaussianExpression([28,28]), LearnedGaussianExpression([10,10]), num_genes),
+        nn.Tanh(),
+        ParameterizedXOXLinear(LearnedGaussianExpression([10,10]), LearnedExpression(10), num_genes)
+    )
+    return train(net, mnist, max_batches=steps,title='learned_gaussian_to_learned_gaussian_to_learned', log_dir=None)
+
+@cached
+def train_mnist_multi_layer_learned_gaussian_to_learned_gaussian_to_random(num_genes, steps=5000):
+    net = nn.Sequential(
+        ParameterizedXOXLinear(LearnedGaussianExpression([28,28]), LearnedGaussianExpression([10,10]), num_genes),
+        nn.Tanh(),
+        ParameterizedXOXLinear(LearnedGaussianExpression([10,10]), RandomExpression(10), num_genes)
+    )
+    return train(net, mnist, max_batches=steps,title='learned_gaussian_to_learned_gaussian_to_random', log_dir=None)
+
 
 @cached
 def train_mnist_multi_layer_random_basis(ndims, steps=5000):
@@ -72,41 +100,23 @@ def train_mnist_multi_layer_random_basis(ndims, steps=5000):
 
 
 print('Loading data')
-ranges = 5
 
-for i in [5,10,20,30,40,50]:
-    print(f"Training spatial to spatial to spatial with {i} genes")
-    for j in range(ranges):
+for i in range(1,30):
+    print(f"Training with {i} genes")
+    for j in range(10):
         with indent:
             print(f"Run number {j}")
             train_mnist_multi_layer_spatial_to_spatial_to_spatial(num_genes = i,global_seed = j)
-
-for i in [5,10,20,30,40,50]:
-    print(f"Training spatial_to_spatial_to_learned with {i} genes")
-    for j in range(ranges):
-        with indent:
-            print(f"Run number {j}")
             train_mnist_multi_layer_spatial_to_spatial_to_learned(num_genes = i,global_seed = j)
-
-
-for i in [5,10,20,30,40,50]:
-    print(f"Training random_to_random_to_random with {i} genes")
-    for j in range(ranges):
-        with indent:
-            print(f"Run number {j}")
             train_mnist_multi_layer_random_to_random_to_random(num_genes = i,global_seed = j)
-
-
-for i in [5,10,20,30,40,50]:
-    print(f"Training learned_to_learned_to_learned with {i} genes")
-    for j in range(ranges):
-        with indent:
-            print(f"Run number {j}")
             train_mnist_multi_layer_learned_to_learned_to_learned(num_genes = i,global_seed = j)
+            train_mnist_multi_layer_learned_gaussian_to_learned_gaussian_to_learned(num_genes = i,global_seed = j)
+            train_mnist_multi_layer_learned_gaussian_to_learned_gaussian_to_random(num_genes = i,global_seed = j)
+            train_mnist_multi_layer_spatial_to_spatial_to_random(num_genes=i,global_seed = j)
 
-for i in [10, 50, 100, 200, 500, 1000]:
+for i in np.logspace(np.log10(10),np.log10(1000),num = 30,dtype='int'):
     print(f"Training with {i}-dimensional subspace")
-    for j in range(ranges):
+    for j in range(10):
         with indent:
             print(f"Run number {j}")
             train_mnist_multi_layer_random_basis(i, global_seed=j)
@@ -115,22 +125,39 @@ print('Done')
 
 results_normal = train_mnist_multi_layer_normal()
 
-results_spatial_to_spatial_to_spatial = load_cached_results_as_pandas(spatial_to_spatial_to_spatial)
+results_spatial_to_spatial_to_spatial = load_cached_results_as_pandas(train_mnist_multi_layer_spatial_to_spatial_to_spatial)
 results_spatial_to_spatial_to_spatial['label'] = 'results_spatial_to_spatial_to_spatial'
 
-results_spatial_to_spatial_to_learned = load_cached_results_as_pandas(spatial_to_spatial_to_learned)
+results_spatial_to_spatial_to_learned = load_cached_results_as_pandas(train_mnist_multi_layer_spatial_to_spatial_to_learned)
 results_spatial_to_spatial_to_learned['label'] = 'results_spatial_to_spatial_to_learned'
 
-results_random_to_random_to_random = load_cached_results_as_pandas(random_to_random_to_random)
+results_spatial_to_spatial_to_random = load_cached_results_as_pandas(train_mnist_multi_layer_spatial_to_spatial_to_random)
+results_spatial_to_spatial_to_random['label'] = 'results_spatial_to_spatial_to_random'
+
+results_random_to_random_to_random = load_cached_results_as_pandas(train_mnist_multi_layer_random_to_random_to_random)
 results_random_to_random_to_random['label'] = 'results_random_to_random_to_random'
 
-results_learned_to_learned_to_learned = load_cached_results_as_pandas(learned_to_learned_to_learned)
+results_learned_to_learned_to_learned = load_cached_results_as_pandas(train_mnist_multi_layer_learned_to_learned_to_learned)
 results_learned_to_learned_to_learned['label'] = 'results_learned_to_learned_to_learned'
+
+results_learned_gaussian_to_learned_gaussian_to_random = load_cached_results_as_pandas(train_mnist_multi_layer_learned_gaussian_to_learned_gaussian_to_random)
+results_learned_gaussian_to_learned_gaussian_to_random['label'] = 'results_learned_gaussian_to_learned_gaussian_to_random'
+
+results_learned_gaussian_to_learned_gaussian_to_learned = load_cached_results_as_pandas(train_mnist_multi_layer_learned_gaussian_to_learned_gaussian_to_learned)
+results_learned_gaussian_to_learned_gaussian_to_learned['label'] = 'results_learned_gaussian_to_learned_gaussian_to_learned'
 
 results_random_basis = load_cached_results_as_pandas(train_mnist_multi_layer_random_basis)
 results_random_basis['label'] = 'results_random_basis'
 
-results = pd.concat([results_spatial_to_spatial_to_spatial,results_spatial_to_spatial_to_learned,results_random_to_random_to_random,results_random_basis,results_learned_to_learned_to_learned])
+results = pd.concat([
+    results_spatial_to_spatial_to_spatial,
+    results_spatial_to_spatial_to_learned,
+    results_spatial_to_spatial_to_random,
+    results_random_to_random_to_random,
+    results_random_basis,
+    results_learned_gaussian_to_learned_gaussian_to_learned,
+    results_learned_gaussian_to_learned_gaussian_to_random,
+    results_learned_to_learned_to_learned])
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
